35579e015dacebd09696d403b5282593a5a60c8f
xavidominguez <xavibcn1991@gmail.com>
2019-04-27 11:26:19 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+//  FUNCIONES
+
+
+
+
+// VARIABLES
+
+
+
+
+// BINDS Y EVENTOS
+var card1 = document.querySelector('#card1');
+card1.addEventListener( 'click', function() {
+  card1.classList.toggle('is-flipped');
+});
+var card2 = document.querySelector('#card2');
+card2.addEventListener( 'click', function() {
+  card2.classList.toggle('is-flipped');
+});
+var card3 = document.querySelector('#card3');
+card3.addEventListener( 'click', function() {
+  card3.classList.toggle('is-flipped');
+});
+var card4 = document.querySelector('#card4');
+card4.addEventListener( 'click', function() {
+  card4.classList.toggle('is-flipped');
+});
+var card5 = document.querySelector('#card5');
+card5.addEventListener( 'click', function() {
+  card5.classList.toggle('is-flipped');
+});
+var card6 = document.querySelector('#card6');
+card6.addEventListener( 'click', function() {
+  card6.classList.toggle('is-flipped');
+});
